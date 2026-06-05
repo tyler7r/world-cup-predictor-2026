b802478419d241e991f5af332d87d9635c7971f9
Tyler Randall <tyler7r@gmail.com>
2026-06-05 15:15:21 -0400

    Add result

--- app/pool-entry/page.tsx
+++ app/pool-entry/page.tsx
@@ -5,9 +5,11 @@ import PoolEntryPage from "./PoolComponent";
 export default async function PoolContainer() {
   const sql = neon(process.env.DATABASE_URL!);
   const result =
-    await sql`SELECT COUNT(*) as count FROM users WHERE entered_pool = true`;
+    (await sql`SELECT COUNT(DISTINCT id) as count FROM users WHERE entered_pool = true`) as {
+      count: number;
+    }[];
   const participantCount = result[0].count;
-  const totalPot = participantCount * 10;
+  const totalPot = Number(participantCount) * 10;
 
   return (
     <PoolEntryPage totalPot={totalPot} participantCount={participantCount} />
